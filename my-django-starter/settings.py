4ffdaf5efd2779f124852f8db6e13a88a55b914d
Ahmed Lemine <ahmed.lemine@yahoo.com>
2022-08-22 20:52:35 +0400

    recent posts template mod, css and htmx links

--- my-django-starter/settings.py
+++ my-django-starter/settings.py
@@ -131,9 +131,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
