f6669457fa8fcb3a7f9a3fb560977904a5db06a2
AL <ahmed.lemine@yahoo.com>
2023-09-02 23:32:53 +0400

    edited allowed hosts

--- src/settings.py
+++ src/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.10.55']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
